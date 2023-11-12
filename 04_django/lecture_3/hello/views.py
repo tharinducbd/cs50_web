8190a6fb65b7c45d52eb3b0f4eee8d50d5dde968
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-11-12 05:52:19 +0530

    renamed the hello view

--- 04_django/lecture_3/hello/views.py
+++ 04_django/lecture_3/hello/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def say_hello(request):
     return HttpResponse("Hello, world from Django!")
