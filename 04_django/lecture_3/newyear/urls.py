02614df313db89e6e59addbb31686bf1338bf9ca
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-11-25 05:51:05 +0530

    app_name defined

--- 04_django/lecture_3/newyear/urls.py
+++ 04_django/lecture_3/newyear/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'newyear'    # helps avoid namespace conflicts in urls inside templates
+
 urlpatterns = [
     path("", views.index, name="index")
 ]
