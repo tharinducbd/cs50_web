21512b7683e254ad3ae0100b5439ac9e26de8efb
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-12-18 06:46:04 +0530

    fkeys for origin and dest

--- 05_db_models_sql/airline/flights/models.py
+++ 05_db_models_sql/airline/flights/models.py
@@ -10,8 +10,8 @@ class Airport(models.Model):
 
 
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
     def __str__(self) -> str:
