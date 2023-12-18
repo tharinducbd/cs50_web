6a92d7edf7936a3558472138f49fd76aed15f7be
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-12-18 06:41:36 +0530

    Airport model added

--- 05_db_models_sql/airline/flights/models.py
+++ 05_db_models_sql/airline/flights/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.city} ({self.code})"
+
+
 class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
