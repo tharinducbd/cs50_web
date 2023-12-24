77dffd780451746d6481f0f8dd7f81d30ae77a24
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-12-24 06:18:32 +0530

    view added: flight

--- 05_db_models_sql/airline/flights/views.py
+++ 05_db_models_sql/airline/flights/views.py
@@ -8,3 +8,10 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    # 'pk' is same as 'id', which is a django's sqlite feature
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
