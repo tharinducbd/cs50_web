b661d39ff41f939265c8955dc8a19d428d4162e3
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-01-13 08:16:23 +0530

    new views: index, login_request

--- 05_db_models_sql/airline/users/views.py
+++ 05_db_models_sql/airline/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponsePermanentRedirect
+from django.urls import reverse
+
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponsePermanentRedirect(reverse("login"))
+
+
+def login_request(request):
+    return render(request, "user/login.html")
