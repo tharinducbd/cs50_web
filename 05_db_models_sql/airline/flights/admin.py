630d29b496172f27132bc60a0580e2de23dc25fe
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-01-13 07:10:41 +0530

    Admin modifications

--- 05_db_models_sql/airline/flights/admin.py
+++ 05_db_models_sql/airline/flights/admin.py
@@ -4,6 +4,14 @@ from .models import Flight, Airport, Passenger
 
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights", )
+
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
