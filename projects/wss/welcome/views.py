c48eeb07d9c5e873a7dc08cbaeabfee0654a896d
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-01-23 06:20:30 +0530

    first render

--- projects/wss/welcome/views.py
+++ projects/wss/welcome/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def hello(request):
-    return HttpResponse("Hello, WSS!")
+    return HttpResponse("Hello, world from WSS!")
+
+
+def greet(request, name):
+    return HttpResponse(f"Hi, {name.capitalize()}! \nWelcome to WSS App.")
+
+
+def index(request):
+    return render(request, "welcome/index.html")
