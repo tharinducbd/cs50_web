7621ff64d812fb9d7727bf7002d4817ec78ed4de
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-01-24 06:43:13 +0530

    url: scope/<name>

--- projects/wss/welcome/urls.py
+++ projects/wss/welcome/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("hello", views.hello, name='hello'),
     path("hello/<str:name>", views.greet, name='greet'),
     path("", views.index, name='index'),
+    path("scope/<str:name>", views.greet_2, name='greet_2'),
 ]
