b7d28dc536cba274ba9e8bab96bab5942bade79f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-01-26 05:55:24 +0530

    index view, draft 1

--- projects/wss/idea_logger/views.py
+++ projects/wss/idea_logger/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
+ideas = [
+    "Including every possible component",
+    "An option to incorporate the size at least approximately",
+    "Auto-generate the inputs required",
+    "The ability to define new components",
+    "The ability to define new inputs for the defined components",
+]
+
+
 # Create your views here.
+def index(request):
+    return render(request, "idea_logger/index.html", {
+        "ideas": ideas
+    })
