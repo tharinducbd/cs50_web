13bdd5ff6cbdad9e75710d88e138497e5bc0f25f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-01-27 06:28:02 +0530

    app_name variable added

--- projects/wss/idea_logger/urls.py
+++ projects/wss/idea_logger/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 
+# app_name help avoid namespace conflicts if 'name' of paths are same in multiple apps!
+app_name = "idea_logger"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add_idea"),
