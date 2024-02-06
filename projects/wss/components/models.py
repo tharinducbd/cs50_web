065f14cd19920cd2850f2376ac5bf94b2b49214f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-06 05:49:49 +0530

    morning session end

--- projects/wss/components/models.py
+++ projects/wss/components/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Tank(models.Model):
+    capacity = models.IntegerField()
+    tank_type = models.CharField(max_length=32)
+
+
+class Material(models.Model):
+    material = models.CharField(max_length=64)
