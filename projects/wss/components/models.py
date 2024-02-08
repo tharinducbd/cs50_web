27000f9aa79befc20f132fd3e0adfd87aac61613
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-08 05:40:44 +0530

    Feb 8th 2024

--- projects/wss/components/models.py
+++ projects/wss/components/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Tank(models.Model):
     capacity = models.IntegerField()
-    tank_type = models.CharField(max_length=32)
+    scheme_name = models.CharField(max_length=32)
 
 
 class Material(models.Model):
