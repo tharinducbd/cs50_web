f221c85375e5e4512cb365dac676a0b6cfe9b02f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-09 05:28:30 +0530

    Feb 9th

--- projects/wss/components/models.py
+++ projects/wss/components/models.py
@@ -4,7 +4,11 @@ from django.db import models
 # Create your models here.
 class Tank(models.Model):
     capacity = models.IntegerField()
-    scheme_name = models.CharField(max_length=32)
+    scheme_name = models.CharField(max_length=64)
+    tank_name = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.id}: {self.tank_name} of {self.scheme_name} - {self.capacity}"
 
 
 class Material(models.Model):
