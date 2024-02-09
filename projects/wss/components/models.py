693e6b58a5c886ecbd6ec1956655928339d71294
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-09 05:55:47 +0530

    foreign keys

--- projects/wss/components/models.py
+++ projects/wss/components/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 
 # Create your models here.
-class Tank(models.Model):
-    capacity = models.IntegerField()
-    scheme_name = models.CharField(max_length=64)
-    tank_name = models.CharField(max_length=64)
-
-    def __str__(self) -> str:
-        return f"{self.id}: {self.tank_name} of {self.scheme_name} - {self.capacity}"
-
-
 class Material(models.Model):
     material = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.material}"
+
+
+class Tank(models.Model):
+    capacity = models.IntegerField()
+    contruction_material = models.ForeignKey(Material,
+                                             on_delete=models.CASCADE,
+                                             related_name="tank_materials")
+
+    def __str__(self) -> str:
+        return f"{self.id}: {self.capacity}"
