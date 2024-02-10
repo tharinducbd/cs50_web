1316354ad8901bf720a41345a5a43927c6c4a07c
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-10 05:42:16 +0530

    index v1

--- projects/wss/components/views.py
+++ projects/wss/components/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Tank
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the component maker.")
+    return render(request, "components/index.html", {
+        "tanks": Tank.objects.all()
+    })
