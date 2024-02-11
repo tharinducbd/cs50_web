28172743c531f435c003f777212d0c51929eddda
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-11 05:56:47 +0530

    url, view, template for /tanks/tank_id

--- projects/wss/components/urls.py
+++ projects/wss/components/urls.py
@@ -7,4 +7,5 @@ app_name = "components"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("tanks/<int:tank_id>", views.tank, name="tank"),
 ]
--- projects/wss/components/views.py
+++ projects/wss/components/views.py
@@ -9,3 +9,10 @@ def index(request):
     return render(request, "components/index.html", {
         "tanks": Tank.objects.all()
     })
+
+
+def tank(request, tank_id):
+    tank = Tank.objects.get(id=tank_id)
+    return render(request, "components/tank.html", {
+        "tank": tank
+    })
