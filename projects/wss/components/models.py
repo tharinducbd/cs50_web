ed259b7bad78a0579b3859d96945643bdc19384c
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-12 05:53:53 +0530

    bool fields

--- projects/wss/components/models.py
+++ projects/wss/components/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+# Yes/ No for boolean selects.
+BOOL_CHOICES = (
+    (True, 'Yes'),
+    (False, 'No')
+)
+
+
 # Create your models here.
 class Material(models.Model):
     material_name = models.CharField(max_length=64)
@@ -15,6 +22,12 @@ class Tank(models.Model):
                                              on_delete=models.CASCADE,
                                              related_name="tank_materials")
     color = models.CharField(max_length=12)
+    has_ball_valve = models.BooleanField(choices=BOOL_CHOICES, default=False)
+    has_overflow = models.BooleanField(choices=BOOL_CHOICES, default=False)
+    has_washout = models.BooleanField(choices=BOOL_CHOICES, default=False)
+    has_chamber = models.BooleanField(choices=BOOL_CHOICES, default=False)
+    has_bulkmeter = models.BooleanField(choices=BOOL_CHOICES, default=False)
+    
 
     def __str__(self) -> str:
         return f"{self.id}: {self.capacity}m3 {self.construction_material}"
