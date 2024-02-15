3fec7eaf1d3337217647ca389494bdf2e7f72852
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-15 06:19:10 +0530

    Feb 15th: TP model added

--- projects/wss/components/admin.py
+++ projects/wss/components/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Material, Tank, Scheme
+from .models import Material, Tank, Scheme, TreatmentProcess
 
 # Register your models here.
 admin.site.register(Material)
 admin.site.register(Tank)
+admin.site.register(TreatmentProcess)
 admin.site.register(Scheme)
--- projects/wss/components/models.py
+++ projects/wss/components/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
-# Yes/ No for boolean selects.
+# Choices
 BOOL_CHOICES = ((True, 'Yes'),(False, 'No'))
+TREATMENT_TYPE = [
+    ("PT_INT", "Pre-treatment: Intake"),
+    ("PT_WTP", "Pre-treatment: WTP"),
+    ("AE", "Aeration"),
+    ("SF", "Sand Filtration"),
+    ("DI", "Disinfection"),
+    ("OTHER", "Other Treatment")
+]
 
 
 # Create your models here.
@@ -25,15 +33,23 @@ class Tank(models.Model):
     has_chamber = models.BooleanField(choices=BOOL_CHOICES, default=False)
     has_bulkmeter = models.BooleanField(choices=BOOL_CHOICES, default=False)
     
-
     def __str__(self) -> str:
         return f"{self.id}: {self.capacity}m3 {self.construction_material}"
+
+
+class TreatmentProcess(models.Model):
+    treatment_type = models.CharField(max_length=100, choices=TREATMENT_TYPE, default="OTHER")
+    process_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"Model class of {self.process_name}"
 
 
 class Scheme(models.Model):
     scheme_name = models.CharField(max_length=64)
     district = models.CharField(max_length=32)
     tanks = models.ManyToManyField(Tank, blank=True, related_name="schemes")
+    treatment = models.ManyToManyField(TreatmentProcess, blank=True, related_name="schemes")
 
     def __str__(self) -> str:
         return f"{self.district} {self.scheme_name}"
