1ad08ec1bda5009c4a8133993732e5bdcb2f793c
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-16 05:11:36 +0530

    index: added schemes and tps

--- projects/wss/components/views.py
+++ projects/wss/components/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Tank
+from .models import Scheme, Tank, TreatmentProcess
 
 
 # Create your views here.
 def index(request):
     return render(request, "components/index.html", {
-        "tanks": Tank.objects.all()
+        "tanks": Tank.objects.all(),
+        "schemes": Scheme.objects.all(),
+        "treatment_processes": TreatmentProcess.objects.all()
     })
 
 
