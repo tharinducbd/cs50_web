886e75b8d71bd760f91fa43911115607c8ca6a62
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-19 06:23:58 +0530

    Admin edits: cool!

--- projects/wss/components/admin.py
+++ projects/wss/components/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 
 from .models import Material, Tank, Scheme, TreatmentProcess
 
+
+class TankAdmin(admin.ModelAdmin):
+    list_display = ("capacity", "construction_material", "has_bulkmeter")
+
+class SchemeAdmin(admin.ModelAdmin):
+    filter_horizontal = ("list_treatments",)
+
 # Register your models here.
 admin.site.register(Material)
-admin.site.register(Tank)
+admin.site.register(Tank, TankAdmin)
 admin.site.register(TreatmentProcess)
-admin.site.register(Scheme)
+admin.site.register(Scheme, SchemeAdmin)
