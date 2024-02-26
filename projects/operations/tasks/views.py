3c579e49785512714fc07df201a607184303731c
tharinducbd <tharinduchanaka@gmail.com>
2024-02-26 11:26:37 +0530

    views.formclass is working!

--- projects/operations/tasks/views.py
+++ projects/operations/tasks/views.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import Component, Component_Type, Task
@@ -24,10 +25,12 @@ def add_task(request):
 class AddTaskForm(forms.Form):
     list_components = Component.objects.all().order_by('component_type', 'component')
 
-    # component = forms.ChoiceField(choices=list_components, label="Component")
-    task_name = forms.CharField(label="Task", max_length=100)
-    # task_interval = forms.ChoiceField(choices=dict(INTERVALS))
-    # task_responsibility = forms.ChoiceField(choices=dict(RESPONSIBILITY))
+    component = forms.ModelChoiceField(queryset=list_components,
+                                       label="Component")
+    task_name = forms.CharField(label="Task",max_length=200, initial="Enter task")
+    task_interval = forms.ChoiceField(choices=INTERVALS, label="Task interval")
+    task_responsibility = forms.ChoiceField(choices=RESPONSIBILITY, label="Responsibility")
+    # refer documentation on form fields!
 
 
 def add_task_2(request):
