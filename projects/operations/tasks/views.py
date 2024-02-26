d31563a599c4e28a6dd6ece43c5d7a6e7feee043
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-02-26 06:18:38 +0530

    add_2: incomplete!

--- projects/operations/tasks/views.py
+++ projects/operations/tasks/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render
 
 from .models import Component, Component_Type, Task
@@ -12,9 +13,24 @@ def index(request):
 
 
 def add_task(request):
-    components = Component.objects.all()
+    list_components = Component.objects.all().order_by('component_type', 'component')
     return render(request, 'tasks/add_task.html', {
-        "components": components,
+        "components": list_components,
         "intervals": dict(INTERVALS),
         "responsibilities": dict(RESPONSIBILITY),
+    })
+
+
+class AddTaskForm(forms.Form):
+    list_components = Component.objects.all().order_by('component_type', 'component')
+
+    # component = forms.ChoiceField(choices=list_components, label="Component")
+    task_name = forms.CharField(label="Task", max_length=100)
+    # task_interval = forms.ChoiceField(choices=dict(INTERVALS))
+    # task_responsibility = forms.ChoiceField(choices=dict(RESPONSIBILITY))
+
+
+def add_task_2(request):
+    return render(request, 'tasks/add_task_2.html',{
+        "form": AddTaskForm()
     })
