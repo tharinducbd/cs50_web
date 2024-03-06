7c8d841c909a43557cf3d5c92538d05ad5dd4fad
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-03-06 06:01:23 +0530

    AddComponentForm()

--- projects/operations/tasks/forms.py
+++ projects/operations/tasks/forms.py
@@ -4,18 +4,33 @@ from .models import Component, Component_Type, Task
 from .models import INTERVALS, RESPONSIBILITY
 
 
+class AddComponentForm(forms.Form):
+    list_ctypes = Component_Type.objects.all().order_by('component_type')
+    list_ctypes_formatted = []
+    for item in list_ctypes:
+        temp_tuple = (item.id, item)
+        list_ctypes_formatted.append(temp_tuple)
+
+    component_type = forms.ChoiceField(choices=list_ctypes_formatted)
+    component = forms.CharField(label="Component name",
+                                max_length=64,
+                                widget=forms.TextInput(attrs={
+                                    'placeholder': 'Enter component name',
+                                    'columns': 64,
+                                }))
+
+
 class AddTaskForm(forms.Form):
     list_components = Component.objects.all().order_by('component_type', 'component')
-
-    my_list = []
+    list_components_formatted = []
     for item in list_components:
         temp_tuple = (item.id, item)
-        my_list.append(temp_tuple)
+        list_components_formatted.append(temp_tuple)
 
     # component = forms.ModelChoiceField(queryset=list_components,
     #                                    label="Component",
     #                                    empty_label="Select component")
-    components = forms.MultipleChoiceField(choices=my_list)
+    components = forms.MultipleChoiceField(choices=list_components_formatted)
     task_name = forms.CharField(label="Task",
                                 max_length=200,
                                 # initial="Enter task", << 'delete n enter' kind of placeholder.
