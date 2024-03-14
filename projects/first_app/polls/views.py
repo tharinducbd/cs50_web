44a522db6ea9240ce733ebbd45678c361334363a
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-03-14 05:37:14 +0530

    detail, results, vote views: barebones

--- projects/first_app/polls/views.py
+++ projects/first_app/polls/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = f"Your're lookin at the results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
