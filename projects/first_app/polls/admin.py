128792a794a26b8258ec59910a59f164ba80cf3a
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-03-26 05:54:27 +0530

    ChoiceAdmin created

--- projects/first_app/polls/admin.py
+++ projects/first_app/polls/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+
+class ChoiceAdmin(admin.ModelAdmin):
+    ordering = ("question", "choice_text",)
+    list_display = ("choice_text", "question", "votes")
+
+
 admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
