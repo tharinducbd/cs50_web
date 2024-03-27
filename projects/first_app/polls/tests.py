96d1d399ca399c9792798913ced91fc35f344c2c
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-03-27 07:08:45 +0530

    session'd

--- projects/first_app/polls/tests.py
+++ projects/first_app/polls/tests.py
@@ -49,4 +49,10 @@ def create_question(question_text, days):
 
 
 class QuestionIndexViewTests(TestCase):
-    pass
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
