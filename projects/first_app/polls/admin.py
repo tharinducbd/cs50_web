b68c6793c8de93a8f0cdd6f04ea8c9d40def24ee
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-03-30 05:51:56 +0530

    QuestionAdmin added

--- projects/first_app/polls/admin.py
+++ projects/first_app/polls/admin.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    ordering = ("id",)
+    list_display = ("id", "question_text", "pub_date",)
+
 class ChoiceAdmin(admin.ModelAdmin):
     ordering = ("question", "choice_text",)
     list_display = ("choice_text", "question", "votes")
 
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice, ChoiceAdmin)
