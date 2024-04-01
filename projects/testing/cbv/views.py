076d2915094b72bd4dabd28d0fe4a3a5ee072bec
Tharindu Dissanayake <65799387+tharinducbd@users.noreply.github.com>
2024-04-01 10:02:07 +0000

    Ex2View()

--- projects/testing/cbv/views.py
+++ projects/testing/cbv/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from typing import Any
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from cbv.models import Post
+
+
+class Ex2View(TemplateView):
+
+    """TemplateResponseMixin
+    Provides a mechanism to construct a TemplateResponse, given suitable context.
+    Attributes:
+    """
+    template_name = "cbv/ex2.html"
+    # template_engine = The NAME of a template engine to use for loading the template. (Jinja2, Genshi)
+    # response_class = Custom template loading or custom context object instantiation.
+    # content_type = Default Django uses 'text/html'
+
+    """ get_context_data(**kwargs) is a method inherited from ContentMixin. """
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.get(id=1)
+        context['data'] = "Context Data fro Ex2"
+        return context
