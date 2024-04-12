f49a527296ac0cc56715058150ab7be716a24f6f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-04-12 06:40:03 +0530

    polls path updated

--- projects/first_app/first_app/urls.py
+++ projects/first_app/first_app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("polls/", include("django_polls.urls")),
     path('__debug__/', include("debug_toolbar.urls")),
     path('admin/', admin.site.urls),
 ]
