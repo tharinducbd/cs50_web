5ccddb33825edb28c491efc93938c126c898b975
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-04-25 21:01:40 +0530

    hello, count working

---
+++ 06_javascript/try_again_1/01_hello.js
@@ -0,0 +1,31 @@
+// hello() to swithc between hello and goodbye
+// counter() to count from zero
+
+function hello() {
+    const heading = document.querySelector('h3');
+    const change_button = document.querySelector('#change');
+    if (heading.innerHTML === 'Hello!') {
+        heading.innerHTML = 'Goodbye!';
+        change_button.innerHTML = 'Back to hi';
+    }
+    else {
+        heading.innerHTML = 'Hello!';
+        change_button.innerHTML = 'All done.';
+    }
+}
+
+
+let counter = 0;
+
+function count() {
+    counter++;
+    document.querySelector('h4').innerHTML = counter;
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('#change').onclick = hello;
+    document.querySelector('#count').onclick = count;
+
+});
