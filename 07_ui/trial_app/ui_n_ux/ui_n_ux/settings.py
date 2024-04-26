c29a1aa829570587c14176c9c7a9fa6303a7fe86
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-04-26 19:46:44 +0530

    learn_ui app configured

--- 07_ui/trial_app/ui_n_ux/ui_n_ux/settings.py
+++ 07_ui/trial_app/ui_n_ux/ui_n_ux/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'learn_ui.apps.LearnUiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
