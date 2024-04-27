ac8d81545bf9cadbc50b920111fce6e970d299be
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-04-27 05:37:46 +0530

    bug fix: return ralse added

--- 06_javascript/try_again_1/02_form.js
+++ 06_javascript/try_again_1/02_form.js
@@ -3,10 +3,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector('#f_1').onsubmit = function () {
-        let msg = document.querySelector('h5');
         const name = document.querySelector('#name').value;
 
-        msg.innerHTML = `Welcome, ${name}!`;
+        document.querySelector('h5').innerHTML = `Welcome, ${name}!`;
+        return false;
     };
 
 });
