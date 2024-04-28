48b5a5fb7ceca3ba9cb4f7838d6c61025141a855
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-04-29 05:03:39 +0530

    Apr 29th

---
+++ 06_javascript/try_again_1/03_colors.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // Change fort color to red
+    document.querySelector('#red').onclick = function () {
+        document.querySelector('#heading_1').style.color = 'red';
+    };
+});
