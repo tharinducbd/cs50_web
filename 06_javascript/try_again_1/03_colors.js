b43e76379c1aba94811e56166606446aae947da6
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-04-29 05:08:52 +0530

    the 'basic' method

--- 06_javascript/try_again_1/03_colors.js
+++ 06_javascript/try_again_1/03_colors.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     
-    // Change fort color to red
+    // Change font color to red
     document.querySelector('#red').onclick = function () {
         document.querySelector('#heading_1').style.color = 'red';
+    };
+    // Change font color to green
+    document.querySelector('#green').onclick = function () {
+        document.querySelector('#heading_1').style.color = 'green';
+    };
+    // Change font color to blue
+    document.querySelector('#blue').onclick = function () {
+        document.querySelector('#heading_1').style.color = 'blue';
     };
 });
