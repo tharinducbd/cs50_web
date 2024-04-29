a035ffe6b17b7edaa12d5f7270e847ae6aad885f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-04-29 05:36:33 +0530

    qSAll(), forEach

--- 06_javascript/try_again_1/03_colors.js
+++ 06_javascript/try_again_1/03_colors.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
-    // Change font color to red
+
+    // Method 1: Not so effective due to repetition...
     document.querySelector('#red').onclick = function () {
         document.querySelector('#heading_1').style.color = 'red';
     };
-    // Change font color to green
     document.querySelector('#green').onclick = function () {
         document.querySelector('#heading_1').style.color = 'green';
     };
-    // Change font color to blue
     document.querySelector('#blue').onclick = function () {
         document.querySelector('#heading_1').style.color = 'blue';
     };
+
+    // Method 2: 'data' attribute
+    document.querySelectorAll('.btns_gr_2').forEach(function(button) {
+        button.onclick = function() {
+            document.querySelector('#heading_2').style.color = button.dataset.color;
+        }
+    });
 });
