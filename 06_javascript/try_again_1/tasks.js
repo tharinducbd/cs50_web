52dfbc381e615b112121b255d3dec8fddbd9ae17
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-02 06:01:18 +0530

    bug fix: no empty tasks allowed

--- 06_javascript/try_again_1/tasks.js
+++ 06_javascript/try_again_1/tasks.js
@@ -1,6 +1,18 @@
 // Scripts for to-do app
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    // By default, submit button is disabled (until we type something on New Task field).
+    document.querySelector('#submit_task').disabled = true;
+
+    document.querySelector('#task').onkeyup = () => {
+        if (document.querySelector('#task').value.length > 0) {
+            document.querySelector('#submit_task').disabled = false;
+        } else {
+            document.querySelector('#submit_task').disabled = true;
+        }
+    };
+
     document.querySelector('#add_task_form').onsubmit = () => {
         const task = document.querySelector('#task').value;
         // console.log(task);
@@ -9,6 +21,9 @@ document.addEventListener('DOMContentLoaded', () => {
         li.innerHTML = task;
 
         document.querySelector('#todolist').append(li);
+
+        document.querySelector('#task').value = '';
+        document.querySelector('#submit_task').disabled = true;
 
         // Stop form from submitting
         return false;
