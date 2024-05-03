31135d194198cb585f683278957e1f588aa1a5a4
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-03 05:41:34 +0530

    localStorage

--- 06_javascript/try_again_1/01_hello.js
+++ 06_javascript/try_again_1/01_hello.js
@@ -15,15 +15,21 @@ function hello() {
 }
 
 
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0)
+}
 
 function count() {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h4').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('h4').innerHTML = localStorage.getItem('counter');
 
     document.querySelector('#change').onclick = hello;
     document.querySelector('#count').onclick = count;
